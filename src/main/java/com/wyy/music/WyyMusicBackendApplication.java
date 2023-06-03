4d96a7a28fb1a1095da46ac43cb7a69181c73e23
hongxiaobin <2812181610@qq.com>
2023-06-03 09:49:13 +0800

    Initial commit

---
+++ src/main/java/com/wyy/music/WyyMusicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wyy.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyyMusicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WyyMusicBackendApplication.class, args);
+    }
+
+}
