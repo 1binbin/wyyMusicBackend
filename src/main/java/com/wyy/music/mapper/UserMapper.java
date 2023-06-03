f6df2b12e26c31d93724a85579d4d4a98b1bb018
hongxiaobin <2812181610@qq.com>
2023-06-03 10:29:17 +0800

    feat: 添加目录

---
+++ src/main/java/com/wyy/music/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.wyy.music.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyy.music.domain.User;
+
+/**
+ * @Author hongxiaobin
+ * @Time 2023/6/3 0003-10:02:59
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
